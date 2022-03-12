546a9015c682cc65b630db6d5dbee13e745cb51a
kdavit <davitkurcxalia7@gmail.com>
2022-03-12 20:17:21 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import speech_recognition as sr
+import pyttsx3
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 150)
+    engine.say(text)
+    engine.runAndWait()
+
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said
+
+
+text = get_audio()
+if "hello" in text:
+    speak("hello someone")
