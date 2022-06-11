608b912fc2127b4641731288badebbcfd51e9128
Pkhovelishvili <103783902+Pkhovelishvili@users.noreply.github.com>
2022-06-11 13:35:31 +0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -7,11 +7,10 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 
-def weather(city): # მეორე date-ის პარამეტრიც დავამატოთ
+def weather(city,date):
     city += "+weather"
-    # city = city.replace(" ", "+")
     res = requests.get(
-        f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',
+        f'https://www.google.com/search?q={city}+{date}&oq={city}+{date}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',
         headers=headers)
     print("Searching...\n")
     soup = BeautifulSoup(res.text, 'html.parser')
@@ -19,8 +18,7 @@ def weather(city): # მეორე date-ის პარამეტრიც 
     time = soup.select('#wob_dts')[0].getText().strip()
     info = soup.select('#wob_dc')[0].getText().strip()
     weather = soup.select('#wob_tm')[0].getText().strip()
-    collecttext = time + location + weather + info
-    # collecttext = "in " + location + " at " + time + " it's " + info + " and temperature is " + weather + "°C"
+    collecttext = "in " + location + " at " + time + " it's " + info + " and temperature is " + weather + "°C"
     translator = Translator()
     translated_text = translator.translate(collecttext)
     return translated_text.text
