14c578e567c82c5a2c42d6aeed09194d52414856
zuka <zuka.filfani1999@gmail.com>
2022-06-13 17:18:01 +0400

    We took out the functions speak and get_audio

--- main.py
+++ main.py
@@ -8,31 +8,8 @@ from wrtieNote import note
 from weather import weather
 from listenAndSpeak import *
 
-# check and write down note
-def chekPraseForNote(text):
-    speak("What you like me to write down ?")
-    note_text = get_audio()
-    note(note_text)
-    speak("I've made a note of that.")
-
-
-# check and read pdf book
-def chekPraseForPDF(text):
-    PDFREAD_STRS = ["pdf", "read", "reed"]
-    for phrase in PDFREAD_STRS:
-        if phrase in text:
-            transformPDFtoAudio(speak)
-
 def hello(text):
     return "hello someone"
-
-
-#
-
-# chekPraseForNote(text)
-# chekPraseForPDF(text)
-# chekPraseForJock(text)
-
 
 def runAssistent():
     text = get_audio()
