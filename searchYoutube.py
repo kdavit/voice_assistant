39645e0ab87f116c09f4bc68a0379a31fec7df37
kh1g <92939389+kh1g@users.noreply.github.com>
2022-07-07 00:10:39 +0400

    Update searchYoutube.py

--- searchYoutube.py
+++ searchYoutube.py
@@ -4,10 +4,11 @@ import webbrowser
 
 
 def searchYoutube(text):
-    search_keyword = text
+    search_keyword = text.replace(" ","+")
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-    print()
+    print('aqa var')
 
-    get_url = webbrowser.open("https://www.youtube.com/watch?v=" + video_ids[0])
-    print("here it is")
+    webbrowser.open("https://www.youtube.com/watch?v=" + video_ids[0])
+
+    return "here it is"
