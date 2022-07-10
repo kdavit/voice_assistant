5be52314d6cf992477efe59df1257b3e34d4fd95
zuka <zuka.filfani1999@gmail.com>
2022-07-10 16:09:40 +0400

    add exception for youtube search

--- searchYoutube.py
+++ searchYoutube.py
@@ -4,11 +4,12 @@ import webbrowser
 
 
 def searchYoutube(text):
-    search_keyword = text.replace(" ","+")
-    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
-    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-    print('aqa var')
-
-    webbrowser.open("https://www.youtube.com/watch?v=" + video_ids[0])
+    try:
+        search_keyword = text.replace(" ", "+")
+        html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+        webbrowser.open("https://www.youtube.com/watch?v=" + video_ids[0])
+    except:
+        return "please say search words"
 
     return "here it is"
